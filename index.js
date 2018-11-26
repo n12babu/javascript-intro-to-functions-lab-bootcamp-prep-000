72c85a7327847e24d9bf8393b17be20e5a3595cf
Nazrul Babu <n12babu@siena.edu>
2018-11-26 17:12:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+const sayHiToGrandma= (string)=>{
+  if ((string.toLowercase())=== string){
+    return "I can't hear you!"
+  }else if ((string.toUppercase())=== string){
+  return "YES INDEED!"
+  }else if (string==="I love you, Grandma."){
+    return "I love you, too."
+  }
+};
